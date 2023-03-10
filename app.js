99346a785c6878293905dc2ef8b30f8b5a948897
flavil <flavia.lon@icloud.com>
2023-03-10 19:13:24 +0100

    adapt listening port

--- app.js
+++ app.js
@@ -130,6 +130,6 @@ app.post("/delete", function(req, res) {
     }
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server listening on port 3000");
 });
